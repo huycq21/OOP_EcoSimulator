6541ccf2babc381174fcedca78d8cba7b16bbe4c
Lã Thanh Tùng <lathanhtung@MacBook-Air-cua-La.local>
2026-04-30 20:53:32 +0700

    new

--- src/model/AnimalState.java
+++ src/model/AnimalState.java
@@ -1,5 +1,11 @@
 package model;
 
 public enum AnimalState {
-
+    WANDERING,  // Đi lang thang/ngẫu nhiên
+    CHASING,    // Đuổi theo con mồi
+    FLEEING,    // Bỏ chạy khỏi kẻ thù
+    EATING,     // Đang ăn
+    SLEEPING,   // Đang ngủ (hồi năng lượng)
+    HIDING,     // Đang trốn trong bụi rậm
+    DEAD        // Đã chết (chờ thành Carcass hoặc bị xóa)
 }
