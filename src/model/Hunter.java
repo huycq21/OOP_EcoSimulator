1709d055d04c3aa98f01ae752cf5cfcc59acba04
Lã Thanh Tùng <lathanhtung@MacBook-Air-cua-La.local>
2026-04-30 20:47:09 +0700

    remove

--- src/model/Hunter.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-public interface Hunter {
-    // Hành vi tấn công mục tiêu
-
-    void hunt(Entity target); 
-    int getNutritionalValue();
-    // Kẻ đi săn cũng có thể bị ăn
-}
