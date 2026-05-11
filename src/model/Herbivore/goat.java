d670996964be7eadc8383bac4dccd3f39051e7f2
Lã Thanh Tùng <lathanhtung@MacBook-Air-cua-La.local>
2026-05-11 22:41:47 +0700

    new

--- src/model/Herbivore/goat.java
+++ src/model/Herbivore/goat.java
@@ -8,7 +8,7 @@ public class Goat extends Herbivore {
     public Goat(Vector2D position) {
         // Kích thước: 4.0, Máu: 60, Năng lượng: 120
         // Tốc độ: 6.5, Tầm nhìn: 70.0
-        super(position, 4.0, 60, 120, 6.5, 70.0);
+        super(position, 4.0, 61, 120, 6.5, 70.0);
         this.setBrain(new PassiveStrategy());
     }
 }
