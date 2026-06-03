f268ebbdb06fc216cead127882f940f2fe8c42f5
Huy Cao Quang <huyvinh2106@gmail.com>
2026-06-04 03:41:17 +0700

    correct path

---
+++ assets/Environment/Forest/Exterior_objetcts2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="Exterior_objetcts2" tilewidth="16" tileheight="16" tilecount="540" columns="15">
+ <image source="../craftpix-net-685720-2d-pixel-fishing-village-pack-dock-interior-boats-npcs/PNG/Exterior_objetcs.png" width="240" height="576"/>
+</tileset>
---
+++ assets/Environment/Forest/Exterior_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="Exterior_tiles" tilewidth="16" tileheight="16" tilecount="336" columns="14">
+ <image source="../craftpix-net-685720-2d-pixel-fishing-village-pack-dock-interior-boats-npcs/PNG/Exterior_tiles.png" width="224" height="384"/>
+</tileset>
